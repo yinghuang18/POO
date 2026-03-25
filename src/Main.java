bbaa3dc80a42929be9d369fac9da259db5ecc8af
POO <y.huang01@ufromail.cl>
2026-03-25 10:06:38 -0300

    codigo inicial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Dame un número tope: ");
+        int numeroFinal = Integer.parseInt(scanner.nextLine());
+        for (int numeroInicio = 0; numeroInicio <= numeroFinal; numeroInicio++) {
+            System.out.println(numeroInicio);
+        }
+
+
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
+        }
+    }
