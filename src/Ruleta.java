b23562b155b17f21990bbfb5df9fabee60c0a968
POO <y.huang01@ufromail.cl>
2026-03-26 09:48:40 -0300

    tipo de apuesta

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -24,24 +24,10 @@ public class Ruleta {
 
     public int mostrarMenu() {
         System.out.println("Elegir una opcion:");
-        String opcion = scanner.nextLine();
-
-
-
-
-    }
-     Scanner scanner = new Scanner(System.in);
-    public int tipoApuesta(){
-         System.out.println("Seleccione un tipo de apuesta 1 o 2: /n 1. rojo/negro /n par/impar");
-         int opcion = scanner.nextInt();
-         return opcion;
-    }
-    public String leerTipoApuesta(int opcion) {
-        if (opcion == 1) {
-            return "rojo/negro";
-        } else {
-            return "par/impar";
-        }
+        String respuesta = scanner.nextLine();
+        int opcion = Integer.parseInt(respuesta);
+        return opcion;
     }
 }
+
 
