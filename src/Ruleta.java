2fef8c125c1d396ff6b50e588d5e8d04ae884c7f
POO <y.huang01@ufromail.cl>
2026-03-27 15:52:27 -0300

    llerTipoApuesta()

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -1,31 +1,31 @@
 import java.util.Random;
 import java.util.Scanner;
 public class Ruleta {
-    public final int Max_historial = 100;
-    public int[] hitorialNumeros = new int[Max_historial];
-    public int[] hitorialApuestas = new int[Max_historial];
-    public boolean[] historialAciertos = new boolean[Max_historial];
-    public int historialSize = 0;
+    public static final int Max_historial = 100;
+    public static int[] hitorialNumeros = new int[Max_historial];
+    public static int[] hitorialApuestas = new int[Max_historial];
+    public static boolean[] historialAciertos = new boolean[Max_historial];
+    public static int historialSize = 0;
 
-    public Random rng = new Random();
-    public int[] numerosRojos = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
+    public static Random rng = new Random();
+    public static int[] numerosRojos = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         menu();
     }
 
-    public void menu() {
+    public static void menu() {
         Scanner scanner = new Scanner(System.in);
-        int opcion;
+        int opcionMenu;
         do {
             mostrarMenu();
-            opcion = leerOpcion(scanner);
-            ejecutarOpcion(opcion, scanner);
-        } while (opcion != 4);
+            opcionMenu = leerOpcion(scanner);
+            ejecutarOpcion(opcionMenu, scanner);
+        } while (opcionMenu != 4);
 
     }
 
-    public void mostrarMenu() {
+    public static void mostrarMenu() {
 
         System.out.println("Opciones a elegir: ");
         System.out.println("1. Iniciar ronda");
@@ -34,12 +34,39 @@ public class Ruleta {
         System.out.println("4. Salir");
     }
 
-    public int leerOpcion(Scanner scanner) {
-        int opcionSelecionada = scanner.nextInt();
-        return opcionSelecionada;
+    public static int leerOpcion(Scanner scanner) {
+        int opcionMenuSelecionada = scanner.nextInt();
+        return opcionMenuSelecionada;
     }
-    public static void ejecutarOpcion(int opcion, Scanner in) {
 
+    public static void ejecutarOpcion(int opcionMenu, Scanner scanner) {
+        switch(opcionMenu) {
+            case 1:
+                iniciarRonda(scanner);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
+
+    }
+    public static void iniciarRonda(Scanner scanner) {
+        leerTipoApuesta(scanner);
+
+
+    }
+    public static char leerTipoApuesta(Scanner scanner) {
+        System.out.println("Seleccione tipo de apuesta:");
+        System.out.println("P. Par");
+        System.out.println("I. Impar");
+        System.out.println("R. Rojo");
+        System.out.println("N. Negro");
+        char opcionApuesta = scanner.next().toUpperCase().charAt(0);
+        return opcionApuesta;
 
     }
 
