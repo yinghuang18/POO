eb1e1284ebb93df812a268e603a1037a3724dcbc
POO <y.huang01@ufromail.cl>
2026-04-08 20:20:47 -0400

    mostrarVentana()

--- src/VentanaRegistro.java
+++ src/VentanaRegistro.java
@@ -4,7 +4,25 @@ public class VentanaRegistro {
     private final JFrame frame = new JFrame("Registro - Casino Black Cat");
     private final JLabel lblUsuario = new JLabel("Usuario:");
     private final JTextField txtUsuario = new JTextField();
+    private final JLabel lblNombre = new JLabel("Usuario:");
+    private final JTextField txtNombre = new JTextField();
     private final JLabel lblClave = new JLabel("Clave:");
     private final JPasswordField txtClave = new JPasswordField();
-    private final JButton btnIngresar = new JButton("Registrar");
+    private final JButton btnRegistrar = new JButton("Registrar");
+
+    public VentanaRegistro() {
+        frame.add(lblUsuario);
+        frame.add(txtUsuario);
+        frame.add(lblNombre);
+        frame.add(txtNombre);
+        frame.add(lblClave);
+        frame.add(txtClave);
+        frame.add(btnRegistrar);
+
+    }
+    public void mostrarVentana() {
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
