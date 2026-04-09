85fbda991b1df84cbe1f93c4bfa4b46b89a5f048
POO <y.huang01@ufromail.cl>
2026-04-08 22:18:30 -0400

    completé la ventana Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,2 +1,7 @@
 public class Launcher {
+    public static void main (String[] args) {
+        VentanaLogin login = new VentanaLogin();
+        login.mostrarVentana();
+        Ruleta ruleta = new Ruleta();
+    }
 }
