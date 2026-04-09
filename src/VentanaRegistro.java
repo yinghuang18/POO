f43ed9b89139457c48298954cad9ff40ba563d6c
POO <y.huang01@ufromail.cl>
2026-04-08 20:14:56 -0400

    se reutilizó parte del codigo de la ventanaLogin

--- src/VentanaRegistro.java
+++ src/VentanaRegistro.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class VentanaRegistro {
+    private final JFrame frame = new JFrame("Registro - Casino Black Cat");
+    private final JLabel lblUsuario = new JLabel("Usuario:");
+    private final JTextField txtUsuario = new JTextField();
+    private final JLabel lblClave = new JLabel("Clave:");
+    private final JPasswordField txtClave = new JPasswordField();
+    private final JButton btnIngresar = new JButton("Registrar");
 }
