49abf634784afc356494b99515e81f78d2305c76
POO <y.huang01@ufromail.cl>
2026-05-28 09:33:05 -0400

    se agregaron funciones y atributo en Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,23 +1,31 @@
-
 public class Usuario {
     private String username;
     private String password;
     private String nombre;
+    private int saldo;
+
+
     public Usuario(String username, String password, String nombre) {
         this.username = username;
         this.password = password;
         this.nombre = nombre;
+        this.saldo = 0;
     }
 
-    // Verifica si las credenciales coinciden
     public boolean validarCredenciales(String u, String p) {
         return this.username.equals(u) && this.password.equals(p);
     }
     public String getNombre() {
-
         return nombre;
     }
+    public int getSaldo(){
+        return this.saldo;
+    }
+    public void sumarSaldo(int monto) {
+        this.saldo += monto;
+    }
+    public void restarSaldo(int monto) {
+        this.saldo -= monto;
+    }
 }
-
-
 
