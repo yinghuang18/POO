de09172a4fe61796a9aa089c990ce62008c55f1e
POO <y.huang01@ufromail.cl>
2026-06-17 13:42:15 -0400

    test rachaYtipoMasJugado

--- src/test/java/pruebasRuleta.java
+++ src/test/java/pruebasRuleta.java
@@ -1,5 +1,8 @@
 import Modelo.Apuestas.ApuestaImpar;
+import Modelo.Apuestas.ApuestaNegro;
+import Modelo.Estadisticas;
 import Modelo.RepositorioEnMemoria;
+import Modelo.Resultado;
 import Modelo.Ruleta;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +35,24 @@ public class pruebasRuleta {
         Ruleta ruleta = new Ruleta(2000,new RepositorioEnMemoria());
         assertThrows(IllegalArgumentException.class, () -> {ruleta.jugar(new ApuestaImpar(3000));
         });
+
+    }
+    @Test
+    void rachaYtipoMasJugado(){
+        RepositorioEnMemoria repositorioEnMemoria = new RepositorioEnMemoria();
+        Estadisticas estadisticas= new Estadisticas(repositorioEnMemoria);
+
+        repositorioEnMemoria.guardar(new Resultado(23,"ROJO",false,4000,new ApuestaNegro(2000)));
+        repositorioEnMemoria.guardar(new Resultado(4,"PAR",false,2000,new ApuestaImpar(1000)));
+        repositorioEnMemoria.guardar(new Resultado(5,"IMPAR",true,1000,new ApuestaImpar(2000)));
+        repositorioEnMemoria.guardar(new Resultado(7,"IMPAR",true,3000,new ApuestaImpar(3000)));
+        repositorioEnMemoria.guardar(new Resultado(6,"PAR",false,6000,null ));
+        assertEquals(2, estadisticas.getVictorias());
+        assertEquals(2, estadisticas.getRachaMaxima());
+        assertEquals(50.0, estadisticas.getPorcentajeVictorias());
+        assertEquals("IMPAR", estadisticas.getTipoMasJugado());
+
+
 
     }
 }
